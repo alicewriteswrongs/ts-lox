2af9898d4ece732db0e627217c5bd5512cc50449
Alice Pote <alice.writes.wrongs@gmail.com>
2022-03-23 16:43:53 -0400

    starting to build out the Expr class

---
+++ src/Expr.ts
@@ -0,0 +1,14 @@
+abstract class Expr {
+}
+
+class Binary extends Expr {
+  left: Expr
+  operator: Token
+  right: Expr
+
+  constructor(left: Expr, operator: Token, right: Expr) {
+    this.left = left
+    this.operator = operator
+    this.right = right
+  }
+}
